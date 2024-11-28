4d50ff18fde50bd65a3c26d2b8f6324eacb78eac
DewitteRuben <rubendewitte1998@gmail.com>
2024-11-28 13:12:41 +0100

    feat: rehydrate Decimal instances after it comes back from the backend

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,10 @@
+import Decimal from "decimal.js";
+import { StatementRecord } from "../../../shared/types";
+
 class StatementProcessorAPI {
   private baseURL: string = "http://localhost:3000/api";
 
-  async validate(file: File) {
+  async validate(file: File): Promise<StatementRecord[]> {
     const formData = new FormData();
     formData.append("statement_record", file);
 
@@ -10,7 +13,17 @@ class StatementProcessorAPI {
       body: formData,
     }).then((res) => res.json());
 
-    return data;
+    // Rewrap the data, converting the proper fields into Decimal objects
+    const wrappedData: StatementRecord[] = data.map(
+      (record: StatementRecord) => ({
+        ...record,
+        startBalance: new Decimal(record.startBalance),
+        mutation: new Decimal(record.mutation),
+        endBalance: new Decimal(record.endBalance),
+      })
+    );
+
+    return wrappedData;
   }
 }
 
