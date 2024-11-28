8dace91a5db444332f5dd5ba21c43757a844a67d
DewitteRuben <rubendewitte1998@gmail.com>
2024-11-28 12:20:43 +0100

    chore: initialize express with cors

--- backend/src/index.test.ts
+++
@@ -1,5 +0,0 @@
-describe("helloWorld function", () => {
-  it('should return "Hello, World!"', () => {
-    expect("Hello, World!").toBe("Hello, World!");
-  });
-});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
