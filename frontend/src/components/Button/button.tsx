7a84dfd50aa0cc33cd41d1102f170c6402e97b64
DewitteRuben <rubendewitte1998@gmail.com>
2024-11-28 12:12:02 +0100

    chore: update button styles

--- frontend/src/components/Button/button.tsx
+++ frontend/src/components/Button/button.tsx
@@ -8,7 +8,7 @@ type ButtonProps = {
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 focus:outline-none dark:focus:ring-orange-800"
       onClick={onClick}
     >
       {text}
