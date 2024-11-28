0a1934c7ca5569ee31ab8001277f10d1ec3934fe
DewitteRuben <rubendewitte1998@gmail.com>
2024-11-28 13:15:25 +0100

    chore: update home page and include records list

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import FileValidationMananager from "../components/FileValidationManager/filevalidationmanager";
+import { StatementRecord } from "../../../shared/types";
+import { StatementRecordList } from "../components/StatementRecordList/statementrecordlist";
 
 const Home = () => {
+  const [statementRecords, setStatementRecords] = useState<StatementRecord[]>(
+    []
+  );
+
   return (
     <div className="container mx-auto">
       <h1 className="text-lg font-bold mb-4">
         Rabobank Customer Statement Validator
       </h1>
-      <FileValidationMananager />
+      <FileValidationMananager onValidate={setStatementRecords} />
+      <div>
+        <StatementRecordList records={statementRecords} />
+      </div>
     </div>
   );
 };
